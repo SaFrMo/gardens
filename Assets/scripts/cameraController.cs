21bb878f176f11740c8434cfe3ce89414afbe805
Caleb <caleb3221@gmail.com>
2014-04-21 12:27:34 -0400

    adding sprites, unimplemented pixelsnap material, and scenes

---
+++ Assets/scripts/cameraController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraController : MonoBehaviour {
+	private float cameraDepth = -10; //store the Z position of the camera
+	private float cameraX = 0; // in case we want to move the camera later
+	public GameObject player;
+	// Use this for initialization
+	void Start () {
+
+		//Set camera's orthographic size to ensure that the scene displays the same on screens with different resolutions
+		camera.orthographicSize = (Screen.height / 100f / 2.0f); // 100f is the PixelPerUnit that you have set on your sprite. Default is 100.
+	}
+	
+	// Update is called once per frame
+	void Update () { 
+		//Update the camera to follow the player's Y position while staying still horizontally and vertically
+		transform.position = new Vector3(cameraX, player.transform.position.y, cameraDepth);
+
+	}
+}
---
+++ Assets/scripts/movement/playerController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerController : MonoBehaviour {
+	public float maxSpeed = 10;
+	private bool facingRight = true;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+
+	void ApplyGravity()
+	{
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float move = Input.GetAxis("Horizontal");
+
+		rigidbody2D.velocity = new Vector2(move * maxSpeed, rigidbody2D.velocity.y);
+
+
+		//make sure the character is facing the right direction
+		if (move > 0 && !facingRight)
+		{
+			Flip ();
+		}
+		else if (move < 0 && facingRight)
+		{
+			Flip ();
+		}
+	}
+
+	//Change which way the character is facing. 
+	void Flip() 
+	{
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1; //inverts x scale to flip sprite
+		transform.localScale = theScale;
+	}
+}
