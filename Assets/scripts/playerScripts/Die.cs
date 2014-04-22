0c5ad6fa9bfda44df6719d516d9d54bb943a4097
Ementiam <ementiam@gmail.com>
2014-04-22 16:25:05 -0400

    updating script locations

---
+++ Assets/scripts/playerScripts/Die.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Die : MonoBehaviour {
+	// Setting this to true will make the player respawn
+	bool shouldRespawn = false;
+
+	//Particle system to use for blood effects
+	public ParticleSystem bloodEffect;
+	//Store the instance of the blood particle system
+	Object bloodInstance;
+	// Use this for initialization
+	void Start () {
+		bloodEffect.enableEmission = false;
+		bloodEffect.Stop();
+	}
+	private void OnCollisionEnter2D (Collision2D c) {
+		// tag all "floor" tiles that should kill the player "Floor"
+		if (c.gameObject.tag == "Floor") {
+			KillPlayer ();
+		
+
+		}
+	}
+	void KillPlayer() {
+
+		//Keep player from moving
+		GetComponent<WASDMovement>().CurrentType = WASDMovement.MovementType.Dead;
+		//Show blood particles
+		bloodEffect.enableEmission = true;
+		bloodEffect.Emit (1000);
+		
+		//wait a few seconds then respawn
+		StartCoroutine( WaitAndRespawn());
+		
+
+
+	}
+	IEnumerator WaitAndRespawn() {
+		Debug.Log ("waiting");
+		yield return new WaitForSeconds(2);
+		Debug.Log ("waited");
+		//setting this to true respawns the player in the next frame
+		shouldRespawn = true;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (shouldRespawn == true) {
+			//respawn at playerStart
+			GetComponent<Respawn>().RespawnPlayer();
+			//make sure player dosen't keep respawning
+			shouldRespawn = false;
+
+			//make sure particles aren't emitting anymore, the simulation has reset and all particles are cleared
+			bloodEffect.enableEmission = false;
+			bloodEffect.Stop();
+			bloodEffect.Clear(); 
+		}
+	}
+}
---
+++ Assets/scripts/playerScripts/Respawn.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Respawn : MonoBehaviour {
+	//use an empty GameObject to set where the player spawns
+	public GameObject spawnPoint;
+
+	//Set this to true to make the player spawn at spawnPoint
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	//Sets movement type back to Grounded from Dead and moves player to the spawnpoint
+	public void RespawnPlayer() {
+		GetComponent<WASDMovement>().CurrentType = WASDMovement.MovementType.Grounded;
+		rigidbody2D.transform.position = spawnPoint.transform.position;
+
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
