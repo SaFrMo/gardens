c1fb93b91146f9cd47218203e2d4000a844e4004
Sander <sandermoolin@gmail.com>
2014-04-23 14:08:46 -0400

    Early flying cars system

---
+++ Assets/scripts/flyingCars/FlyingCarMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlyingCarMovement : MonoBehaviour {
+
+	private float speed;
+
+	private void Start () {
+		speed = UnityEngine.Random.Range (15, 70);
+	}
+
+	private void Update () {
+		transform.Translate (Vector2.right * speed * Time.deltaTime, transform);
+		if (Mathf.Abs(transform.position.x) > 110) {
+			Destroy (gameObject);
+		}
+	}
+}
