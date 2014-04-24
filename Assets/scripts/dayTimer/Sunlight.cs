792030d108ece2878a639494053b7ce66829e189
Sander <sandermoolin@gmail.com>
2014-04-24 12:36:33 -0400

    Started light and shadow system - tell time without a clock GUI

---
+++ Assets/scripts/dayTimer/Sunlight.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sunlight : MonoBehaviour {
+
+	private Light2D sun;
+
+	private void Start () {
+		sun = Light2D.Create (new Vector2 (-32, 19),
+		                      new Color (1, 1, 1, .001f),
+		                      500f);
+	}
+}
---
+++ Assets/scripts/economy1/MoneyChangeNotification.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoneyChangeNotification : MonoBehaviour {
+
+	public int amount;
+
+	private float side = 100f;
+
+	private float startTime;
+
+	void Start () {
+		Destroy (gameObject, 1f);
+		startTime = Time.realtimeSinceStartup;
+	}
+
+	public void SetAmount (int a) {
+		amount = a;
+	}
+
+	void OnGUI () {
+		// TODO: Make this a label
+		GUI.Box (new Rect (Screen.width / 2 - side / 2,
+		                     (Screen.height / 2 - side / 2) - (Time.realtimeSinceStartup - startTime) * 30f,
+		                     side,
+		                     side), amount.ToString());
+	}
+}
