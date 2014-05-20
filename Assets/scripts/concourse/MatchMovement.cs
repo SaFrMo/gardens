75d984c5c3f913182d301bbdc683a3f21d9190c0
Sander <sandermoolin@gmail.com>
2014-05-20 12:26:19 -0400

    Sound work and concourse work

---
+++ Assets/scripts/concourse/MatchMovement.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MatchMovement : MonoBehaviour {
+
+	public GameObject toMatch;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = toMatch.transform.position;
+	}
+}
