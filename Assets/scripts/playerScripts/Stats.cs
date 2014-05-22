caadf1455fef55362bc4c8c73ec1bdafb23b6601
Sander <sandermoolin@gmail.com>
2014-05-22 01:17:16 -0400

    Started XP reward and saving system

---
+++ Assets/scripts/playerScripts/Stats.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[SerializeAll]
+public class Stats : MonoBehaviour {
+
+	public static int ToNextLevel ()
+	{
+		return 1;
+	}
+
+	public static int XP = 0;
+		
+	public static int Level 
+	{ 
+		// TODO: level up slope
+		get { return (int)XP / 5; }
+	}
+}
