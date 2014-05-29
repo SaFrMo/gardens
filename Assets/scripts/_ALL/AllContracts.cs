c026c2d9f5bc582bce3c47ffc7144b638158d36c
Sander <sandermoolin@gmail.com>
2014-05-29 11:30:09 -0400

    Started putting graphics in place

--- Assets/scripts/_ALL/AllContracts.cs
+++ Assets/scripts/_ALL/AllContracts.cs
@@ -4,11 +4,11 @@ using System.Collections.Generic;
 
 public class AllContracts {
 
-	public static Contract chicago1 = new Contract ("Chicago", null, "chicago1", 
+	public static Contract chicago1 = new Contract ("Chicago: Proof of Concept", null, "chicago1", 
 	                                                "A government position managing the experimental gardens on small apartment buildings.",
 	                                                Contract.City.Chicago, true);
-	public static Contract chicago2 = new Contract ("Chicago", null, "chicago1", 
-	                                                "FIRE",
+	public static Contract chicago2 = new Contract ("Chicago: Fire Station", null, "chicago1", 
+	                                                "Work for the CFD on a very visible building front. Raise money for them, catch public attention for your work.",
 	                                                Contract.City.Chicago, false);
 
 
