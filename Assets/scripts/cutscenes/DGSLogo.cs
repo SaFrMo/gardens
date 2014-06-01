6d7d2d1a1056241bbbaebcce8cf1a8199adb311c
Sander <sandermoolin@gmail.com>
2014-05-31 21:40:46 -0400

    Added logo screen, plant renderer location fix

---
+++ Assets/scripts/cutscenes/DGSLogo.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DGSLogo : MonoBehaviour {
+
+	public Texture logo;
+	public string nextLevelName = "mainMenu";
+
+	private void Start ()
+	{
+		Invoke ("GoToNextLevel", 3f);
+	}
+
+	private void GoToNextLevel ()
+	{
+		Application.LoadLevel(nextLevelName);
+	}
+
+	private void OnGUI ()
+	{
+		GUI.DrawTexture (new Rect (Screen.width / 2 - logo.width / 2,
+		                           Screen.height / 2 - logo.height / 2,
+		                           logo.width / 2,
+		                           logo.height), logo);
+	}
+}
